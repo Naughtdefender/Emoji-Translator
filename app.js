89b738c61443510cf7636e5419623834a66fb145
Kshitiz Sharma <kshitizsharma405@gmail.com>
2023-07-16 10:20:21 +0530

    maked the app

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var emojiDictionary = {
   "😊": "Smiling Face with Smiling Eyes",
   "😂": "Face with Tears of Joy",
   "🤣": "Rolling on the Floor Laughing",
-  "❤": "Red Heart"
+  "❤": "Red Heart",
 };
 var emojisKnow = object.keys(emojiDictionary);
 
@@ -14,17 +14,42 @@ export default function App() {
 
 
 
+  const { meaning, setMeaning } = useState("");
 
-    return (
-<div className="App">
-    <h1> Emoji| Translator</h1>
-
-
-
-
-
-
-
-        );
+  function emojiInputHandler(event) {
+      var userInput = event.target.value;
+    var meaning = emojiDictionary[userInput]
+    if (meaning===undefined) {
+        meaning = "We don't have this in our database";
     }
-    
+
+    setMeaning(meaning);  
+
+}
+
+function emojiOnClickHandler(emoji) {
+    var meaning = emojiDictionary[emoji];
+    setMeaning(meaning)
+}
+  return (
+    <div className="App">
+      <h1> Emoji| Translator</h1>
+
+      <input onChange={emojiInputHandler}></input>
+      <div>{meaning} </div>
+      <h3> Emojis we know </h3>
+        {emojisKnow.map(function(emoji) {
+            return <span
+            onClick={() => emojiOnClickHandler(emoji)} 
+            style={{fontSize: "2rem", padding: "0.5rem", pointer: "cursor"}} 
+            key={emoji}>
+                {emoji}
+                </span>
+        })}
+
+
+
+
+    </div>
+  );
+}
